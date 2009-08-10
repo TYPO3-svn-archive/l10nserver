ab865b5dec94604f46647a330c2468b3f39e7f5d
akushnarov <akushnarov@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-10 14:15:03 +0000

    - Deleted old files
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nserver/trunk@23224 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Domain/Model/PartRepository.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Tx_L10nServer_Domain_Model_PartRepository extends Tx_Extbase_Persistence_Repository {
-}
-?>
--- Classes/Domain/Model/ProjectRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Tx_L10nServer_Domain_Model_ProjectRepository extends Tx_Extbase_Persistence_Repository {
-	
-}
-?>
